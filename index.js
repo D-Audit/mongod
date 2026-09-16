fdecfe36ab93ec66dae5a54a71549a2d8369a611
Don Jesus <donjesuskayiranga@gmail.com>
2026-09-16 16:46:59 +0200

    Clean up MongoDB connection code - remove extra whitespace and unnecessary comments

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
-const mongoose = require("mongoose");
-const User = require("./models/user");
-mongoose.connect("mongodb://127.0.0.1:27018/ecommerce", {
-})
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.log("Connection Error:", err));
+const mongoose = require('mongoose');
+const User = require('./models/user');
+
+mongoose.connect('mongodb://127.0.0.1:27018/ecommerce', {})
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log('Connection Error:', err));
+
 async function createUser() {
-    const user = new User({
-        name: "Jesus",
-        email: "jesus@gmail.com"
-    });
-    await user.save();
-    console.log(user);
+  const user = new User({
+    name: 'Jesus',
+    email: 'jesus@gmail.com'
+  });
+  await user.save();
+  console.log(user);
 }
+
 async function getUsers() {
-    const users = await User.find();
-   console.log(users); 
-} 
+  const users = await User.find();
+  console.log(users);
+}
+
 getUsers();
 createUser();
-
-
-
-
-
-
